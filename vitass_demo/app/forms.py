ae32c3b88462c16a7fd42e110925469b5cb415b8
Janis Gailis <janis.gailis@uia.no>
2023-09-22 00:33:30 +0200

    fixed Style check

--- vitass_demo/app/forms.py
+++ vitass_demo/app/forms.py
@@ -1,10 +1,10 @@
 from django import forms
 
-#from martor.fields import MartorFormField
-#from app.models import Post
+# from martor.fields import MartorFormField
+# from app.models import Post
 
 
 class SimpleForm(forms.Form):
     title = forms.CharField(widget=forms.TextInput())
-    #description = MartorFormField()
-    #wiki = MartorFormField()
+    # description = MartorFormField()
+    # wiki = MartorFormField()
--- vitass_demo/app/views.py
+++ vitass_demo/app/views.py
@@ -1,15 +1,17 @@
 from django.conf import settings
 from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
-from django.contrib import messages
+# from django.contrib.auth.decorators import login_required
+# from django.contrib import messages
 
-#from app.forms import SimpleForm, PostForm
-#from app.models import Post
+# from app.forms import SimpleForm, PostForm
+# from app.models import Post
 
 from app.forms import SimpleForm
 
+
 def home_redirect_view(request):
     return redirect("simple_form")
+
 
 def simple_form_view(request):
     form = SimpleForm()
